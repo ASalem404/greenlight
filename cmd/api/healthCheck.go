c15e1854dc0a1402916ca394a7bb978d34c67462
Ahmed Salem <as2720094@gmail.com>
2023-06-26 01:27:16 +0300

    updating healthcheckHandler to send a well-formed JSON response

--- cmd/api/healthCheck.go
+++ cmd/api/healthCheck.go
@@ -8,7 +8,9 @@ import (
 // Declare a handler which writes a plain-text response with information about the
 // application status, operating environment and version.
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "status: available")
-	fmt.Fprintf(w, "environment: %s\n", app.config.env)
-	fmt.Fprintf(w, "version: %s\n", version)
+
+	js := fmt.Sprintf(`"status":"available","environment":%q,"version":%q`, app.config.env, version)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte(js))
 }
